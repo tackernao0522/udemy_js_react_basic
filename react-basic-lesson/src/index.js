cbb5524e9e79fec3ea95dba785008d6ae2b89ce3
takaki <cheap_trick_magic@yahoo.co.jp>
2021-10-31 20:10:19 +0900

    コンポーネントの使い方を知る

--- react-basic-lesson/src/index.js
+++ react-basic-lesson/src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import App from './App.jsx'
 
-const App = () => {
-  return (
-    <>
-      <h1>こんにちは!</h1>
-      <p>お元気ですか？</p>
-    </>
-  )
-}
 
 ReactDom.render(<App />, document.getElementById("root"))
